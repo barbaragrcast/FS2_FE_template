d5bed6af9d6cfa4c3a6277c2b1841daab4eed023
barbaragrcast <barbaragrcast@gmail.com>
2026-02-23 22:51:45 -0600

    run please

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,3 @@
-export { default as NavBar } from "./navBar.jsx";
+export { default as NavBar } from "./nav.jsx";
 export { default as Footer } from "./footer.jsx";
 export { default as Hero } from "./hero.jsx";
