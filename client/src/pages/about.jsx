b8675f57a06970a31cf3990f5cc6c5a2193abb5a
barbaragrcast <barbaragrcast@gmail.com>
2026-03-03 00:14:11 -0600

    text

--- client/src/pages/about.jsx
+++ client/src/pages/about.jsx
@@ -4,8 +4,9 @@ const About = () => {
   return (
     <>
       <div id="about">
-        <h1> About Page </h1>
-        <h3> Add information about your business here!</h3>
+        <h1> About Our Bakery </h1>
+        <h3> Welcome to Bun & Butter Bakery! We bake fresh bread, cookies, brownies, and pastries every day using simple, quality ingredients.
+            Our goal is to bring a little sweetness and warmth to your day. Stop by for something fresh and delicious</h3>
       </div>
     </>
   );
