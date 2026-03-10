fb1f1d6641dc77607b1d8d0b3bc487e6b444292f
barbaragrcast <barbaragrcast@gmail.com>
2026-03-09 20:34:37 -0500

    s

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,7 +23,6 @@ import Home from "./pages/home";
 
 // Components
 import { NavBar, Footer } from "./components/index.js";
-
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
   const [cart, setCart] = useState([]);
@@ -37,44 +36,36 @@ function App() {
   };
 
   return (
-    <BrowserRouter>
-      {/* NavBar */}
-      <NavBar
-        searchTerm={searchTerm}
-        setSearchTerm={setSearchTerm}
-        cartCount={cart.length}
+  <BrowserRouter>
+  <NavBar
+    searchTerm={searchTerm}
+    setSearchTerm={setSearchTerm}
+    cartCount={cart.length}
+  />
+
+  <div className="main">
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/account" element={<Account />} />
+      <Route path="/cart" element={<Cart cart={cart} removeFromCart={removeFromCart} />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route
+        path="/shopping"
+        element={
+          <Shopping
+            searchTerm={searchTerm}
+            addToCart={addToCart}
+            cart={cart}
+          />
+        }
       />
+    </Routes>
+  </div>
 
-      {/* Spacer to offset fixed nav */}
-      <div style={{ height: "100px" }}></div>
-
-      {/* Main content */}
-      <div className="main">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/account" element={<Account />} />
-          <Route
-            path="/cart"
-            element={<Cart cart={cart} removeFromCart={removeFromCart} />}
-          />
-          <Route path="/contact" element={<Contact />} />
-          <Route
-            path="/shopping"
-            element={
-              <Shopping
-                searchTerm={searchTerm}
-                addToCart={addToCart}
-                cart={cart}
-              />
-            }
-          />
-        </Routes>
-      </div>
-
-      <Footer />
-    </BrowserRouter>
+  <Footer />
+</BrowserRouter>
   );
 }
 
